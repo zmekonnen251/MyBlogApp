6ddba1d9c373996cf07830e85ac5fd71d0cf1adc
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-07-13 22:13:59 +0200

    Create a comment model and implement an association with user and post model and implement update_comments_counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
-  belongs_to :post
-  belongs_to :user
+  belongs_to:author , class_name: 'User', foreign_key: 'author_id'
+  belongs_to:post , class_name: 'Post', foreign_key: 'post_id'
+
+  def update_comments_counter
+    Post.find(self.post_id).increment!(:comments_counter)
+  end
+
 end
