8a8b44df431fd47105d93b0e3fe3d48bfc2f981f
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-07-13 22:11:11 +0200

    Create a Like model,implement the association with post and user models and implement update_likes_counter method

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to:author , class_name: 'User', foreign_key: 'author_id'
+  belongs_to:post , class_name: 'Post', foreign_key: 'post_id'
+
+  def update_likes_counter(post)
+    Post.find_by(post_id: post.post_id).increment!(:likes_counter)
+  end
+
 end
