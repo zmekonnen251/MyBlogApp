5c5caa4a298b8e85f6cf7d866060db8204e4cd5c
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-07-13 22:09:06 +0200

    Create a user model,add association with post,like and comment classes and implement most_recent_posts for a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_many :posts,foreign_key: :author_id 
+  has_many :comments,foreign_key: :author_id
+  has_many :likes,foreign_key: :author_id
+
+  def most_recent_posts
+    Post.find_by(author_id: self.id).order(created_at: :desc).limit(3)
+  end
+  
 end
