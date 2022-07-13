d84ad8b74f1a49df2f3e8cf56267b332764c0158
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-07-13 22:56:48 +0200

    Fix linter error

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,8 @@
 class Like < ApplicationRecord
-  belongs_to:author , class_name: 'User', foreign_key: 'author_id'
-  belongs_to:post , class_name: 'Post', foreign_key: 'post_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
-  def update_likes_counter(post)
-    Post.find_by(post_id: self.post_id).increment!(:likes_counter)
+  def update_likes_counter(_post)
+    Post.find_by(post_id: post_id).increment!(:likes_counter)
   end
-
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,10 @@
 class Post < ApplicationRecord
-  has_many :comments,foreign_key: :post_id
-  has_many :likes ,foreign_key: :post_id
+  has_many :comments, foreign_key: :post_id
+  has_many :likes, foreign_key: :post_id
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-
   def update_posts_counter
-    User.find(self.author_id).increment!(:posts_counter)
+    User.find(author_id).increment!(:posts_counter)
   end
 
   def self.most_recent_comments
