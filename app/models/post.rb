fff656f977404d129d07fb414b1088b91e2c1455
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-07-13 22:07:49 +0200

    Create a Post model ,add association with comment,like and user classes and added most_recent_comments & update_posts_counter methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  has_many :comments,foreign_key: :post_id
+  has_many :likes ,foreign_key: :post_id
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+
+  def update_posts_counter
+    User.find(self.author_id).increment!(:posts_counter)
+  end
+
+  def most_recent_comments
+    Comment.find_by(post_id: self.id).order(created_at: :desc).limit(5)
+  end
 end
