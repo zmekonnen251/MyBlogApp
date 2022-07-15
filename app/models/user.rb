1601fbd04893185bf58ca84437a9892f78c2bdc9
zmekonnen251 <zmekonnen251@localhost.localdomain>
2022-07-15 19:39:11 +0300

    Add validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id, dependent: :destroy
   has_many :likes, foreign_key: :author_id, dependent: :destroy
 
-  def self.most_recent_posts
-    Post.order(created_at: :desc).limit(3)
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def most_recent_posts
+    Post.where(author_id: id).order(created_at: :desc).limit(3)
   end
 end
