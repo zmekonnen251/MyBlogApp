83c91f72bb68dad9c877f23cd4c9aac8ebad059d
zmekonnen251 <zmekonnen251@localhost.localdomain>
2022-07-15 19:41:56 +0300

    fix update_likes_counter method

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,6 @@ class Like < ApplicationRecord
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
   def update_likes_counter(_post)
-    Post.find_by(post_id: post_id).increment!(:likes_counter)
+    Post.find_by(post_id).increment!(:likes_counter)
   end
 end
