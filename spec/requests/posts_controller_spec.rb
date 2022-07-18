6f4b30ddc2bee578408e672e0996f3ee8611bb40
Zelalem <mekonnen.zelalem251@gmail.com>
2022-07-18 15:28:14 +0300

    Add a test for checking if the right template rendered

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe 'PostsController', type: :request do
       expect(response).to have_http_status(200)
       expect(response.body).to include('Here is a list of posts for a given user.')
     end
+
+    it 'renders the index.html.erb template' do
+      get '/users/1/posts'
+      expect(response).to render_template('index')
+    end
   end
 
   describe 'GET /users/1/posts/1' do
@@ -14,6 +19,11 @@ RSpec.describe 'PostsController', type: :request do
       get '/users/1/posts/1'
       expect(response).to have_http_status(200)
       expect(response.body).to include('Here is a post for a given user and id of the post.')
+    end
+
+    it 'renders a show.html.erb template' do
+      get '/users/1/posts/1'
+      expect(response).to render_template('show')
     end
   end
 end
