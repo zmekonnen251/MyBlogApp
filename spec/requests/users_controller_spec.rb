acc1c1ff86fcf19a6b621cae3ac9cd67438b45be
Zelalem <mekonnen.zelalem251@gmail.com>
2022-07-18 15:29:02 +0300

    Add a test for checking if the users controller rendered the right template

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe 'UsersController', type: :request do
       expect(response).to have_http_status(200)
       expect(response.body).to include('Here is a list of users in the app.')
     end
+
+    it 'renders the index.html.erb template' do
+      get '/users'
+      expect(response).to render_template('index')
+    end
   end
 
   describe 'GET /users/1' do
@@ -14,6 +19,11 @@ RSpec.describe 'UsersController', type: :request do
       get '/users/1'
       expect(response).to have_http_status(200)
       expect(response.body).to include('Here is a user for a given id.')
+    end
+
+    it 'renders a show.html.erb template' do
+      get '/users/1'
+      expect(response).to render_template('show')
     end
   end
 end
