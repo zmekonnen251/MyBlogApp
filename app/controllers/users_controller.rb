39c6293c5a495269bd9347d4eecc7547f1d9d9bd
Zelalem <mekonnen.zelalem251@gmail.com>
2022-07-20 12:12:06 +0300

    fix user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user_id = current_user.id
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
   get '/users/:user_id/posts', to:'posts#index', as: :user_posts
   get '/users/:user_id/posts/:id', to:'posts#show', as: :user_post
   get '/users', to:'users#index', as: :users
