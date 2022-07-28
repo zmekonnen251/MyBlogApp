b5cba77d24f2509829d3ebba421a33b7f9c90c76
Zelalem <mekonnen.zelalem251@gmail.com>
2022-07-28 14:20:04 +0300

    Add a role column to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_27_204611) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_28_110435) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_27_204611) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "role", default: "default"
     t.index ["posts_counter"], name: "index_users_on_posts_counter", order: :desc
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
