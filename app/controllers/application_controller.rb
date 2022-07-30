2b64efc4e9b7e5d6d4f7cacd1cc342ec2240b8ce
Zelalem <mekonnen.zelalem251@gmail.com>
2022-07-30 05:40:07 +0300

    Add routes for api end points

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
@@ -14,5 +14,20 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(_scope)
     root_path
+  end
+
+  def not_found
+    render json: { error: 'not_found' }
+  end
+
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split.last if header
+    begin
+      @decoded = ApiHelper::JsonWebToken.decode(header)
+      @current_user = User.find_by_email(@decoded[:user_email])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,13 @@ Rails.application.routes.draw do
     end
   end
 
+  post 'api_users/auth/login/', to: 'api/authentication#login', as: :user_login
+  post 'api_users/sign_up', to: 'api/users#create',as: :user_signup
+  get  'api_users/users_list', to: 'api/users#index',as: :list_all_users
+  get  'api_users/get_user', to: 'api/users#show',as: :get_user
+  get  'api_users/:user_id/posts_list', to: 'api/posts#index', as: :list_all_user_posts
+  get  'api_users/:user_id/posts/:post_id/comments_list', to: 'api/comments#index', as: :list_all_user_post_comments
+  post 'api_users/:user_id/posts/:post_id/comments', to: 'api/comments#create', as: :add_comment
 
   root to: 'users#index'
 end
